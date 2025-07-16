73107fd8dfe6ac914268b84695720aeb82b910ec
Daniellaaaaa <onyinyedanielladuru@gmail.com>
2025-07-16 16:28:26 +0100

    Class Task

---
+++ lesson_2.py
@@ -0,0 +1,27 @@
+# Python Short-hand
+first_number=3
+
+first_number += 3 #addition
+print(f"The sum is: {first_number}")
+
+second_number=3
+second_number -= 3 #subtraction
+print(f"The subtraction value is: {second_number}")
+
+first_number *= 3 #multiplication
+print(f"The multiplication value is: {first_number}")
+
+first_number /= 3 #division
+print(f"The division value is: {first_number}")
+
+first_number //= 3 #floor division
+print(f"The floor division value is: {first_number}")
+
+first_number %= 3 #modulus
+print(f"The modulus value is: {first_number}")
+
+
+
+
+
+
